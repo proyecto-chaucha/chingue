10b551d0ea5517c334d2ac224fa459960675d676
César Vásquez C <heellxz@gmail.com>
2018-03-08 02:20:51 -0300

    sistema con cron

--- __main__.py
+++ __main__.py
@@ -1,52 +1,60 @@
-from bitcoinrpc.authproxy import AuthServiceProxy
 from config import RPCuser, RPCpassword, RPCport
-from pymongo import MongoClient
+from bitcoinrpc.authproxy import AuthServiceProxy
+from pymongo import MongoClient, DESCENDING
 from sys import exit
 
-from time import sleep
-
 def main():
+	# MongoDB
 	client = MongoClient()
 	db = client['blockchain']
+	
+	# Collections
 	blocksDB = db['blocks']
 	txDB = db['tx']
+	diffDB = db['diff']
 
+	# JSON-RPC
 	rpc = AuthServiceProxy("http://%s:%s@127.0.0.1:%i"%(RPCuser, RPCpassword, RPCport))
 
+	# Get last values
 	blockCount = rpc.getblockcount()
 	lastBlock = blocksDB.count()
 
-	print("Last block in DB: %i" % (lastBlock - 1))
-	print("Last block in network: %i" % blockCount)
-	print("Tx captured: %i" % txDB.count())
+	# Debug
+	print("- Last block in DB: %i" % (lastBlock - 1))
+	print("- Last block in network: %i" % blockCount)
+	print("- Tx captured: %i" % txDB.count())
 
+	# Capture
 	if blockCount > lastBlock - 1:
 		for i in range(lastBlock, blockCount + 1):
-			block = rpc.getblock(rpc.getblockhash(i))
-			block['difficulty'] = float(block['difficulty'])
-			if 'nextblockhash' in block:
-				del block['nextblockhash']
+			print("- Block #%i" % i)
 
-			print(block['height'], (block['hash']), blocksDB.insert_one(block))
+			# GetBlock
+			block = rpc.getblock(rpc.getblockhash(i))
 
 			if block['height'] > 0:
+				# Remove nextblockhash
+				if 'nextblockhash' in block:
+					del block['nextblockhash']
+
+				# GetTX
 				for j in block['tx']:
 					rawTx = rpc.getrawtransaction(j)
 					tx = rpc.decoderawtransaction(rawTx)
+					
+					# Format value
 					for k in tx['vout']:
-						k['value'] = '{0:.8f}'.format(k['value'])
-					print(tx['txid'], txDB.insert_one(tx))
+						k['value'] = '{0:.8f}'.format(float(k['value']))
 
+					txDB.insert_one(tx) # Insert
+
+				# format difficulty
+				block['difficulty'] = float(block['difficulty'])
+				blocksDB.insert_one(block) # Insert
+
+	print("- Done.")
 
 if __name__ == '__main__':
 	print("UKUKU BLOCKCHAIN EXTRACTOR V 0.1")
-
-	while True:
-		try:
-			main()
-			print("sleeping...")
-			sleep(60)
-
-		except (KeyboardInterrupt, SystemExit):
-			print("vendisuone ~")
-			exit()
+	main()
